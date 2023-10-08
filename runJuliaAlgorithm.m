052211831608065f84e72f6250d6d5cb9749bd1e
richardconnors <richardconnors@gmail.com>
2023-10-08 21:44:42 +0200

    adding TSP trends

--- runJuliaAlgorithm.m
+++ runJuliaAlgorithm.m
@@ -7,36 +7,42 @@
 % check the name of the computer and set the location accordingly
 [~, thisPC] = system ('hostname');
 if strcmpi(strtrim(thisPC),"PP0695") % this is my laptop
-  dirLocation = "C:\Users\richard.connors\Dropbox\_LuxWork\MEVERST\code\TY_Flexbus\";
+  % dirLocation = "C:\Users\richard.connors\Dropbox\_LuxWork\MEVERST\code\TY_Flexbus\";
 else
   dirLocation = "Q:\REPOS\flexbus_v0.6\";
 end
 
-% so now I have the location of the "main folder" I concatenate the
-% filename:
+% so now I have the location of the "main folder" I concatenate the filename:
 % the main JULIA file
-code_main = join([dirLocation, 'example.jl'],""); % join strings with no space
+code_main = append(replace(dirLocation, "\","\\"), 'example.jl'); % join strings with no space
 
-% the directory with all the data files I want to cycle through
-data_dir = join([dirLocation, 'data\non_peak\'],"");
-% grab all the .txt files in this data_dir
-all_data = dir(join([data_dir '*.txt'],""));
-% grab what should be the ONLY .csv file in the folder
-data_timetable = dir(join([data_dir '*.csv'],""));
+% the directory with all the data FOLDERS I want to cycle through
+data_dir = join([dirLocation, 'data\'],"");
 
-% NOTE: the for loop should run TY's JULIA code for each .txt file
-% However I guess each time this would overwrite the results from the
-% previous run!
-% Right now this is not a problem since it doesn't even run once 🤣
-% TO DO: update results .csv files to include name of calling .txt file
+% Use the 'dir' function to list all files and folders in the specified directory
+listing = dir(data_dir);
+% Initialize an empty cell array to store subdirectories matching the pattern
+dataFolders = {};
+% Define the regular expression pattern to match directory names
+pattern = 'P\d+S\d+C\d+DP\d+\.\d+';
+% Iterate through the 'listing' and identify subdirectories matching the pattern
+for i = 1:length(listing)
+    % Check if the item is a directory, not "." or "..", and matches the pattern
+    if listing(i).isdir && ~ismember(listing(i).name, {'.', '..'}) && ~isempty(regexp(listing(i).name, pattern, 'once'))
+        % Add the full path of the matching subdirectory to the cell array
+        dataFolders{end+1} = fullfile(data_dir, listing(i).name);
+    end
+end
+% 'dataFolders' now contains a list of full paths to subdirectories matching the pattern
+disp(dataFolders');
+nDataFolders = length(dataFolders);
 
-for ff = 1%:length(all_data) % for each .txt file in the data_dir
+for ff = 1%:nDataFolders
   % need the path with double \\ for JULIA
-  F1 = join(['"',replace(data_dir, "\","\\"),all_data(ff).name,'"'],"");
-  F2 = join(['"',replace(data_dir, "\","\\"),data_timetable(1).name,'"'],"");
+  F1 = append(replace(dataFolders{1}, "\","\\"),"\\");  
   % run JULIA via the windows command
-  setpwd(dirLocation) % because inside Julia it assumes we are in the REPO
-  [status,cmdout]=system(join(["julia", code_main, F1, F2]))
+  cd(dirLocation) % because inside Julia it assumes we are in the REPO
+  [status,cmdout]=system(join(["julia", code_main, F1]))
 
   %  Here are some hard-coded filenames if needed to check things are
   %  working - you need to set the path for your computer of course!
